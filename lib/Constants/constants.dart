062c681f03ca3d685576d70617c151208810adc2
exonent <scuervomartindatos@gmail.com>
2021-10-19 00:33:20 +0200

    Final

---
+++ lib/Constants/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// ################## Color ##################
+const kButonColor = Color(0xFFC41B60);
+const kGradientStart = Color(0xFFD22A6E);
+const kGradientFinal = Color(0xFFDD3877);
+const kBackgroundColor = Color(0xFFFFFFFF);
+const kTextGrey = Color(0xFFA5A3A3);
+
+// ################## Widgets ##################
+const kButonTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+// ################## Text ##################
+const title = "dribbble";
+const butTonText = "SE CONNECTER";
+const emailHint = "EMAIL";
+const passwordHint = "MOT DE PASSE";
+const loginText = "Vous avez déjà un compte?";
+const loginTextLink = "Inscrivez-vous";
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenge_01_01/Constants/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,205 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      title: 'UI Challenger Day 1',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyHomeChallenge(text: title),
+        backgroundColor: kBackgroundColor,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomeChallenge extends StatelessWidget {
+  const MyHomeChallenge({Key? key, required this.text}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Column(
+      children: [
+        Container(
+          child: (WaveTopPart(text: text)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Padding(
+          padding: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width * 0.12,
+            right: MediaQuery.of(context).size.width * 0.12,
+          ),
+          child: Column(
+            children: [
+              const TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: emailHint,
+                  hintStyle: TextStyle(
+                    color: kTextGrey,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 12,
+                  color: kTextGrey,
+                ),
+              ),
+              const TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                    hintText: passwordHint,
+                    hintStyle: TextStyle(
+                      color: kTextGrey,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye_outlined,
+                      color: Colors.grey,
+                      size: 17,
+                    )),
+                style: TextStyle(
+                  fontSize: 12,
+                  color: kTextGrey,
+                ),
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                child: const Text(
+                  butTonText,
+                  style: TextStyle(
+                    letterSpacing: 2,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  onPrimary: Colors.white,
+                  primary: kButonColor,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  minimumSize: Size(MediaQuery.of(context).size.width, 40),
+                  shadowColor: Colors.transparent,
+                ),
+                onPressed: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                loginText,
+                style: TextStyle(color: kTextGrey),
+              ),
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: loginTextLink,
+                      style: TextStyle(
+                          color: kButonColor,
+                          decoration: TextDecoration.underline),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
+}
+
+class WaveTopPart extends StatelessWidget {
+  const WaveTopPart({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: CustomPathClipper(),
+      child: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 1.5,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [kGradientStart, kGradientFinal],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height / 9,
+              left: MediaQuery.of(context).size.width / 7,
+            ),
+            child: const Image(
+              height: 300,
+              alignment: Alignment.bottomCenter,
+              image: AssetImage('assets/images/dribbleLogo.png'),
+              color: Color.fromRGBO(255, 255, 255, 0.4),
+              colorBlendMode: BlendMode.modulate,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height / 3.5,
+              left: MediaQuery.of(context).size.width / 5,
+            ),
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: kBackgroundColor,
+                fontSize: 70,
+                fontFamily: 'Pacifico',
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomPathClipper extends CustomClipper<Path> {
+  var radius = 10.0;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height / 1.3);
+    //Curva izquierda
+    var firstControlPoint = Offset(size.width / 8 - 20, size.height / 1.3 - 60);
+    var firstEndPoint = Offset(size.width / 4, size.height / 1.3 - 10);
+
+    var secondControlPoint =
+        Offset(size.width - (size.width / 4), size.height / 1.3 + 165);
+    //El height es el canto izquierdo
+    var secondEndPoint = Offset(size.width, size.height / 1.3 + 50);
+
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height / 1.3);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
